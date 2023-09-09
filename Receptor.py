3551116ea06763e0dd45d2d4ba63ccdcc64fbb3f
TheDOMcracker <143950208+TheDOMcracker@users.noreply.github.com>
2023-09-09 18:20:45 -0400

    Update Receptor.py

--- Receptor.py
+++ Receptor.py
@@ -2,16 +2,21 @@
 import socket
 
 host = 'dirección_ip_del_servidor'  # Reemplaza con la dirección IP del servidor
-port = 8443  # Cambia el puerto al 8443 o el puerto que hayas seleccionado
+port = 8443
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 
+print("Conectado a Johangel.")  # Indicar que el otro usuario está conectado
+
 while True:
     data = client_socket.recv(1024).decode()
-    print(f"Johangel: {data}")  # Cambia "Johangel" a [tu nombre]
+    if not data:
+        print("Johangel se ha desconectado.")  # Indicar que el otro usuario se ha desconectado
+        break
+    print(f"Johangel: {data}")  # Cambia "Otro" a "Johangel"
 
-    message = input("Yulianny: ")  # Cambia "Yulianny" a [la persona que recibe]
+    message = input("Yulianny: ")  # Cambia "Tú" a "Yulianny"
     client_socket.send(message.encode())
 
 client_socket.close()
