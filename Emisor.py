214e1be16197b3007d74a92d6f7516e57f543377
TheDOMcracker <143950208+TheDOMcracker@users.noreply.github.com>
2023-09-09 18:20:01 -0400

    Update Emisor.py

--- Emisor.py
+++ Emisor.py
@@ -2,7 +2,7 @@
 import socket
 
 host = '0.0.0.0'  # Escucha en todas las interfaces
-port = 8443  # Cambia el puerto al 8443 o cualquier otro puerto seguro que prefieras
+port = 8443
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
@@ -12,11 +12,15 @@ print(f"Servidor escuchando en {host}:{port}")
 
 client_socket, client_address = server_socket.accept()
 print(f"Conexión entrante desde {client_address}")
+print("Yulianny se ha conectado.")  # Indicar que el otro usuario está conectado
 
 while True:
     message = input("Johangel: ")  # Cambia "Tú" a "Johangel"
     client_socket.send(message.encode())
     data = client_socket.recv(1024).decode()
-    print(f"Yulianny: {data}")  # Cambia "Yulianny" a la persona receptora
+    if not data:
+        print("Yulianny se ha desconectado.")  # Indicar que el otro usuario se ha desconectado
+        break
+    print(f"Yulianny: {data}")  # Cambia "Otro" a "Yulianny"
 
 client_socket.close()
