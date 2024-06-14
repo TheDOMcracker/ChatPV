7bd2f93718505d6cd2686e872a723faf3caf1f25
Johangel Vásquez <143950208+TheDOMcracker@users.noreply.github.com>
2024-06-14 07:19:32 -0400

    Update Receptor.py

--- Receptor.py
+++ Receptor.py
@@ -7,16 +7,15 @@ port = 8443
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host, port))
 
-print("Conectado a Johangel.")  # Indicar que el otro usuario está conectado
+print("Conectado a User1.")  # Indicar que el otro usuario está conectado
 
 while True:
     data = client_socket.recv(1024).decode()
     if not data:
-        print("Johangel se ha desconectado.")  # Indicar que el otro usuario se ha desconectado
+        print("User1 se ha desconectado.")  # Indicar que el otro usuario se ha desconectado
         break
-    print(f"Johangel: {data}")  # Cambia "Otro" a "Johangel"
-
-    message = input("Yulianny: ")  # Cambia "Tú" a "Yulianny"
+    print(f"User1: {data}")  
+    message = input("Tú: ")  
     client_socket.send(message.encode())
 
 client_socket.close()
